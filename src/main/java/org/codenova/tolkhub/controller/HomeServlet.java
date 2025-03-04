d238a27525e2aae8d86334ad66c8c1b41a71814e
soolyn <soolyn4867@gmail.com>
2025-03-04 12:13:22 +0900

    feat : healthcheck

---
+++ src/main/java/org/codenova/tolkhub/controller/HomeServlet.java
@@ -0,0 +1,19 @@
+package org.codenova.tolkhub.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/index")
+public class HomeServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: USER
+  Date: 25. 3. 4.
+  Time: 오전 11:45
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>TalkHub</title>
+</head>
+<body>
+<h1>TalkHub</h1>
+<p>
+    TalkHub 에 오신것을 환영합니다람쥐
+</p>
+
+</body>
+</html>
