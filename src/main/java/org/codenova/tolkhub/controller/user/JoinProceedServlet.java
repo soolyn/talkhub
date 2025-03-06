ec138c0100975906948b33bae88c3ad124f92974
soolyn <soolyn4867@gmail.com>
2025-03-06 09:49:19 +0900

    feat 오류 완

--- src/main/java/org/codenova/tolkhub/controller/user/JoinProceedServlet.java
+++ src/main/java/org/codenova/tolkhub/controller/user/JoinProceedServlet.java
@@ -17,6 +17,8 @@ public class JoinProceedServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        boolean error = false;
+
         String id = req.getParameter("id");
         String password = req.getParameter("password");
         String nickname = req.getParameter("nickname");
@@ -34,13 +36,29 @@ public class JoinProceedServlet extends HttpServlet {
             }
         }
 
-        if(a > 0 && number > 0 || password.length() >= 6){
+        if(id.length() == 0){
+            req.setAttribute("idError", "아이디를 적어주세요");
+            error = true;
+        }else if(id.length() < 3){
+            req.setAttribute("idError", "아이디를 3글자 이상 적어주세요");
+            error = true;}
+
+        if(a > 0 && number > 0 && password.length() >= 6){
+        }else if(password.length() == 0){
+            req.setAttribute("passError", "비밀번호를 입력해주세요");
+            error = true;
         }else{
             req.setAttribute("passError", "비밀번호가 잘못되었습니다(영어 숫자 포함, 6글자 이상)");
+            error = true;
         }
 
         if(nickname.length() == 0){
             req.setAttribute("nickError", "닉네임을 적어주세요)");
+            error = true;
+        }
+
+        if(error){
+            req.getRequestDispatcher("WEB-INF/views/user/join-fail.jsp").forward(req, resp);
         }
 
         boolean result = false;
@@ -55,11 +73,7 @@ public class JoinProceedServlet extends HttpServlet {
 
         } catch (SQLIntegrityConstraintViolationException e) {
             System.out.println(e.toString());
-            if(id.length() == 0) {
-                req.setAttribute("idError", "아이디를 적어주세요");
-            }else{
-                req.setAttribute("idError", "아이디가 중복되었거나 사용할 수 없습니다");
-            }
+            req.setAttribute("idError", "아이디가 중복되었거나 사용할 수 없습니다");
 
         } catch (Exception e) {
             System.out.println("create : " + e.toString());
